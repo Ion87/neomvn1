40936835cf8a42836df859848a3411102d5a8975
IvanLucky <pimop@mail.ru>
2020-04-10 02:28:47 +0300

    cu asteroid

--- src/test/java/tests/NasaProviderTest.java
+++ src/test/java/tests/NasaProviderTest.java
@@ -1,8 +1,20 @@
 package tests;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class NasaProviderTest {
-    @Test
-
+@Test
+    void ceckNasaConnection() throws IOException {
+        URL url = new URL("https://api.nasa.gov/");
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        String inputLine;
+        if ((inputLine = in.readLine())== null){
+            fail("Failed connection to Nasa");
+        }
+    }
 }
