01c0242e24e1d5480430c39bcb5d15f1d8608e80
IvanLucky <pimop@mail.ru>
2020-04-15 17:37:10 +0300

    cu FX

--- src/main/java/main/GUIApplication.java
+++ src/main/java/main/GUIApplication.java
@@ -1,6 +1,28 @@
 package main;
 
-public class GUIApplication {public static void main(String[] args) {
-        System.out.println("GUI rinning");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+
+public class GUIApplication extends Application{
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader();
+        URL url = getClass().getResource("/main.fxml");
+        loader.setLocation(url);
+        Parent root = loader.load();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
